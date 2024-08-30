14c2d6905b0310ef8a0276159277af7263519190
Amir Osama <amirosama21@gmail.com>
2024-08-30 04:00:28 +0300

    Refactor scrapper.py to automate logging in and getting the user's calendar page

--- scrapper.py
+++ scrapper.py
@@ -1,59 +1,116 @@
 import bs4
-from enum import Enum
-import time
 import requests
-from requests_ntlm import HttpNtlmAuth
+import time
+import calendar
+from datetime import date, datetime
 from config import Config
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-BUE_E_LEARNING_URL = "https://learn1.bue.edu.eg/login/index.php"
-
-REDIRECTS_CALENDAR_URL = (
-    f"https://learn1.bue.edu.eg/calendar/view.php?view=month&time={int(time.time())}"
-)
-
-CALENDAR_TABLE_ID = "#month-detailed-66c77365d26d266c77365adeb625"
-
-COURSE_DATA_FILE = "courses.json"
+LOGIN_URL = "https://learn1.bue.edu.eg/login/index.php"
+CALENDAR_URL = "https://learn1.bue.edu.eg/calendar/view.php"
 
 
-class ItemType(str, Enum):
-    ASSIGNMENT = "Assignment Submission"
-    PROJECT = "Phase Submission"
-    OTHERS = "Others"
+def automate_getting_calendar(config: Config) -> requests.Response:
+    """
+    This function automates logging in to the BUE E-Learning using Selenium
+    to get the user specific Calendar page
+
+    Args:
+        config (Config): User Specific Configration file
+
+    Returns:
+        requests.Response: User's Calendar page to be web scraped later
+    """
+
+    # Set up Selenium WebDriver (make sure the WebDriver executable is in your PATH)
+    driver = webdriver.Chrome()  # or use webdriver.Firefox() for Firefox
+
+    # Open the login page
+    driver.get(LOGIN_URL)
+
+    # Perform login
+    username = driver.find_element(By.ID, "username")
+    password = driver.find_element(By.ID, "password")
+    login_button = driver.find_element(By.ID, "loginbtn")
+
+    username.send_keys(config.bue_username)
+    password.send_keys(config.bue_password)
+    login_button.send_keys(Keys.RETURN)
+
+    # Wait for the login to complete
+    time.sleep(1)  # Adjust if needed based on the site
+
+    # Retrieve cookies from Selenium
+    cookies = driver.get_cookies()
+
+    # Convert cookies to a format that `requests` can use
+    cookies_dict = {cookie["name"]: cookie["value"] for cookie in cookies}
+
+    # Close the WebDriver
+    driver.quit()
+
+    # Use cookies with requests
+    session = requests.Session()
+    session.cookies.update(cookies_dict)
+
+    # Make the request to the calendar page with the following query params
+    params = {
+        "view": "month",
+        "time": int(time.time()),
+    }
+    response = session.get(CALENDAR_URL, params=params)
+
+    return response
 
 
-class CourseItem:
-    def __init__(self, item_title: str, item_link: str, item_type: ItemType) -> None:
-        self.title = item_title
-        self.link = item_link
-        self.type = item_type
+def get_submission_date(day) -> str:
+    current_month_name = calendar.month_name[date.today().month]
+    year = datetime.now().year
+    return f"{day}/{current_month_name}/{year}"
 
 
-class Course:
-    material: dict[str, CourseItem]
+def scrape_bue_calendar(config: Config):
+    res = automate_getting_calendar(config)
+    soup = bs4.BeautifulSoup(res.content, "html.parser")
 
-    def __init__(
-        self, code, title, id, semester, material={}, announcements=""
-    ) -> None:
-        self.code = code
-        self.title = title
-        self.id = id
-        self.semester = semester
-        self.material = material
-        self.announcements = announcements
+    # Find all days with submissions
+    days_with_submissions = soup.find_all("td", class_="clickable")
+    if not days_with_submissions:
+        print("No days with submissions found.")
+        return
 
+    submissions = []
+    for day in days_with_submissions:
+        date_element = day.find("a", class_="day")
+        if not date_element:
+            continue
+        date = date_element.text.strip()
 
-class SubmissionScrapper:
-    cached_courses: dict[str, Course]
-    cached_pages: dict[str, bs4.BeautifulSoup] = {}
+        # Find the div containing day-content
+        day_content = day.find("div", {"data-region": "day-content"})
+        if not day_content:
+            continue
 
-    def __init__(self, config: Config) -> None:
-        self.config = config
+        # Find all submission items within this day-content
+        submission_items = day_content.find_all("li", {"data-region": "event-item"})
+        for submission in submission_items:
+            title_element = submission.find("span", class_="eventname")
+            link_element = submission.find("a", {"data-action": "view-event"})
+            if title_element and link_element:
+                submission_details = {
+                    "date": get_submission_date(date),
+                    "title": title_element.text.strip(),
+                    "link": link_element["href"],
+                }
+                submissions.append(submission_details)
 
-    def scrape_bue_page(self, uri):
-        if uri not in SubmissionScrapper.cached_pages:
-            req = requests.get(
-                BUE_E_LEARNING_URL + uri,
-                auth=HttpNtlmAuth(self.config.bue_username, self.config.bue_password),
-            )
-            # TODO: complete the logic of the Scrapper
+    if not submissions:
+        print("No submissions found.")
+    else:
+        for submission in submissions:
+            print(f"Date: {submission['date']}")
+            print(f"Title: {submission['title']}")
+            print(f"Link: {submission['link']}")
+            print("----------")
